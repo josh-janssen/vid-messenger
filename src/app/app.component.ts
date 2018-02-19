d28580479496ae33e8f710d2cda89e8f4d1730bd
Josh Janssen <josh.janssen@gmail.com>
2018-02-19 08:49:30 -0700

    Add first video element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewChildren, QueryList, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  @ViewChild('myname') input:ElementRef; 
+  @ViewChild('video') video:ElementRef;
+  
+  @ViewChildren('div1,div2,div3') divs:QueryList<ElementRef>;
+  
+  ngAfterViewInit() {
+    let _video=this.video.nativeElement;
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+                            .then(stream => {
+                              _video.src = window.URL.createObjectURL(stream);
+                              _video.play();
+                            })
+    }
+
+  }
+
 }
